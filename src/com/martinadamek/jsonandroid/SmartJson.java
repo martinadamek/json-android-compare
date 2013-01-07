96b0dd7843d8b83f0d5f8e0492e31e44dd2041a6
Alexandros Schillings <aschillings@gmail.com>
2013-01-07 13:50:17 +0000

    Removed memory allocations from the parsing loops

--- src/com/martinadamek/jsonandroid/SmartJson.java
+++ src/com/martinadamek/jsonandroid/SmartJson.java
@@ -2,7 +2,11 @@ package com.martinadamek.jsonandroid;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
@@ -10,45 +14,51 @@ import net.minidev.json.parser.JSONParser;
 
 public class SmartJson implements TestJson {
 
-    public String getName() {
-        return "JSON.smart";
-    }
+	public String getName() {
+		return "JSON.smart";
+	}
 
-    public List<Map> parsePublicTimeline(InputStream inputStream) {
+	public List<Map> parsePublicTimeline(InputStream inputStream) {
 
-        List<Map> result = new ArrayList<Map>();
+		List<Map> result = new ArrayList<Map>();
 
-        JSONParser p = new JSONParser(JSONParser.MODE_PERMISSIVE);
-        
-        try {
-            JSONArray jsonArray = (JSONArray) p.parse(new InputStreamReader(inputStream));
-            int size = jsonArray.size();
+		JSONParser p = new JSONParser(JSONParser.MODE_PERMISSIVE);
 
-            for (int i = 0; i < size; i++) {
-                Map map = new HashMap();
-                JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+		try {
+			Map map;
+			Set keys;
+			Set keys2;
+			JSONObject user;
+			JSONObject jsonObject;
+			
+			JSONArray jsonArray = (JSONArray) p.parse(new InputStreamReader(inputStream));
+			int size = jsonArray.size();
+			
+			for (int i = 0; i < size; i++) {
+				map = new HashMap();
+				jsonObject = (JSONObject) jsonArray.get(i);
 
-                Set keys = jsonObject.keySet();
-                for (Object key: keys) {
-                    if ("user".equals(key)) {
-                        JSONObject user = (JSONObject) jsonObject.get(key);
-                        Set keys2 = user.keySet();
-                        for (Object key2: keys2) {
-                            map.put("user." + key2, user.get(key2));
-                        }
-                    } else {
-                        map.put(key, jsonObject.get(key));
-                    }
-                }
+				keys = jsonObject.keySet();
+				for (Object key: keys) {
+					if ("user".equals(key)) {
+						user = (JSONObject) jsonObject.get(key);
+						keys2 = user.keySet();
+						for (Object key2: keys2) {
+							map.put("user." + key2, user.get(key2));
+						}
+					} else {
+						map.put(key, jsonObject.get(key));
+					}
+				}
 
-                result.add(map);
-            }
+				result.add(map);
+			}
 
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        return result;
-    }
+		return result;
+	}
 }
